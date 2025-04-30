9b9462dd20c32e6a8c00b800911adfbe7fd48133
rizqiau <mochammadrizqiaullia@gmail.com>
2025-04-30 10:55:25 +0700

    upload user image

--- src/config/supabaseClient.js
+++ src/config/supabaseClient.js
@@ -3,7 +3,7 @@ const { createClient } = require("@supabase/supabase-js");
 // Ganti dengan data kamu
 const SUPABASE_URL = "https://mkdgwhelfqcodgwktras.supabase.co";
 const SUPABASE_KEY =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1rZGd3aGVsZnFjb2Rnd2t0cmFzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDM5ODgyMTYsImV4cCI6MjA1OTU2NDIxNn0.jGxE3rpGxIXJZCUXbzpLQKIcu4527j25IJ_pjrENiG4"; // anon key kamu
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1rZGd3aGVsZnFjb2Rnd2t0cmFzIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc0Mzk4ODIxNiwiZXhwIjoyMDU5NTY0MjE2fQ.hs1PvKwKItsNGA0hds1hPeezFfTqR-08ovNpHM-UGCs";
 
 const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
 
