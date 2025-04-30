7bc8658eb6e2bc7cfde90ea75eebe60b2b27683d
rizqiau <mochammadrizqiaullia@gmail.com>
2025-04-30 10:43:56 +0700

    upload user image

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.json());
 // Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/plot", plotRoutes);
-app.use("/api/upload", uploadRoutes);
+app.use("/api", uploadRoutes);
 
 // Root route (optional buat cek server nyala)
 app.get("/", (req, res) => {
